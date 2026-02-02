6cc3763047f0d8b518da196b46ccc0cccaa8978f
Kaylie Chang <firenachang@gmail.com>
2026-02-01 17:34:24 -0800

    style: add listing in HTML and adjust hero styling
    - Added bullet points to all sections in portfolio
    - Readjusted size, padding, etc. in the hero section
    - Removed the contacts section completely

--- kaylie-portfolio/app/page.tsx
+++ kaylie-portfolio/app/page.tsx
@@ -27,79 +27,70 @@ export default function HomePage() {
         <div className={styles.heroContent}>
           <div className={styles.heroText}>
             <h1 className={styles.name} id="home">Kaylie Chang</h1>
-            <p className={styles.heroDescription}>Computer Science Student & Aspiring Software Engineer (AI/ML focus)</p>
-            <p className={styles.heroDescription}>Excited to code and learn new things!</p>
+            <p className={styles.heroDescription}>
+              Computer Science Student -- Aspiring Software Engineer (AI/ML)
+            </p>
+            <br />
+            <p className={styles.heroDescription}>
+              Excited to code and learn new things!
+            </p>
           </div>
 
-          <div className={styles.imagePad}>
-            <div className={styles.image}>
-              <img
-                src="/profile_picture.jpeg"
-                alt="Profile Picture of Kaylie Chang in hero"
-                className={styles.profileImage}
-              />
-            </div>
+          <div className={styles.imageWrap}>
+            <img
+              className={styles.heroImage}
+              src="/profile_picture.jpeg"
+              alt="Profile Picture of Kaylie Chang in hero"
+            />
           </div>
-
         </div>
       </section>
 
       {/* Note for later --> emphasize this part the most */}
         <section className={styles.card}>
           <h3 className={styles.sectionTitle} id="projects">Projects</h3>
-          <p className={styles.text}>
-            - This Portfolio Website!
-            <br/>
-            - (Completed - testing) Password Generator Website
-            <br/>
-            - (in progress) Spam Email Classifier using Machine Learning
-          </p>
+          <ul className={styles.bulletPoints}>
+            <li>This Portfolio Website!</li>
+            <li>(Completed - testing) Password Generator Website</li>
+            <li>(in-progress) Spam Email Classifier using Machine Learning</li>
+          </ul>
         </section>
 
       <div className={styles.sections}>
         <section className={styles.card}>
           <h3 className={styles.sectionTitle}>Education</h3>
-          <p className={styles.text}>
-            University of California, Santa Cruz
-            <br/>
-            Computer Science Major
-            <br/>
-            Freshman (Class of 2029)
-          </p>
+          <ul className={styles.bulletPoints}>
+            <li>University of California, Santa Cruz (Class of 2029)</li>
+            <li>B.S. in Computer Science</li>
+            <li>GPA: 4.0/4.0</li>
+            <li>Relevant Courses:
+              CSE 30 (Abstractions of Python Programming),
+              Coursera - Univeristy of Michigan Introduction to HTML5
+            </li>
+            <li>Honors: Dean's List</li>
+          </ul>
         </section>
 
         <section className={styles.card}>
           <h3 className={styles.sectionTitle} id="skills">Skills</h3>
-          <p className={styles.text}>
-            - Programming Languages: Python, Java, HTML, CSS
-            <br/>
-            - Frameworks/Libraries: React, Next.js
-            <br/>
-            - Tools: Git, GitHub, VS Code
-          </p>
+          <ul className={styles.bulletPoints}>
+            <li>Programming Languages: Python, Java, HTML, CSS</li>
+            <li>Frameworks/Libraries: React, Next.js</li>
+            <li>Tools: Git, GitHub, VS Code</li>
+            <li>Others: critical thinking, adaptable/flexible, continuous learning, teamwork, communication</li>
+          </ul>
         </section>
 
         <section className={styles.card}>
           <h3 className={styles.sectionTitle}>Hobbies</h3>
-          <p className={styles.text}>
-            - Playing the guitar
-            <br/>
-            - Dancing
-            <br/>
-            - Reading
-            <br />
-            - Making iMovies
-          </p>
+          <ul className={styles.bulletPoints}>
+            <li>Playing the guitar</li>
+            <li>Dancing</li>
+            <li>Reading</li>
+            <li>Making iMovies</li>
+          </ul>
         </section>
       </div>
-
-      <section className={styles.card}>
-        <h2 className={styles.sectionTitle}>Contact Me</h2>
-        <p className={styles.text}>
-          Email: ...
-        </p>
-      </section>
-
     </main>
     </>
   );
