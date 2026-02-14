46f2ab5cc5987ba33bbac2d47c0e014ea469162d
Kaylie Chang <firenachang@gmail.com>
2026-02-13 23:11:54 -0800

    Switched projects order
    - Made Password Generator to appear first before Portfolio (show bigger and more important projects first)

--- kaylie-portfolio/app/projectsData.ts
+++ kaylie-portfolio/app/projectsData.ts
@@ -2,17 +2,17 @@ import { title } from "process";
 
 export const projects = [
     {
-        title: "Portfolio Website",
-        summary: "A personal portfolio website built using Next.js and TypeScript to showcase my projects and skills.",
-        tech: ["TypeScript", "Next.js", "React", "CSS"],
-        github: "https://github.com/misotofu7/personal_website",
-        demo: "put-video-here"
-    },
-    {
         title: "Password Generator Website",
         summary: "A web application that generates secure passwords based on user-defined criteria such as length and character types.",
         tech: ["JavaScript", "HTML", "CSS"],
         github: "https://github.com/misotofu7/password-generator",
+        demo: "put-video-here"
+    },
+    {
+        title: "Portfolio Website",
+        summary: "A personal portfolio website built using Next.js and TypeScript to showcase my projects and skills.",
+        tech: ["TypeScript", "Next.js", "React", "CSS"],
+        github: "https://github.com/misotofu7/personal_website",
         demo: "put-video-here"
     }
 ];
